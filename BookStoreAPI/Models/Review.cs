60b38af5152353fa9575b27f3b6c03cca74e19f1
BuddhikaNelum <dlbnprabath@gmail.com>
2019-04-19 14:55:04 +0530

    Added data annotaions

--- BookStoreAPI/Models/Review.cs
+++ BookStoreAPI/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,19 @@ namespace BookStoreAPI.Models
 {
     public class Review
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 10, ErrorMessage ="Headline should be between 10 and 200 characters")]
         public string HeadLine { get; set; }
+
+        [Required]
+        [StringLength(2000, MinimumLength = 50, ErrorMessage = "Review Text should be between 10 and 2000 characters")]
         public string ReviewText { get; set; }
+
+        [Required]
+        [Range(1,5,ErrorMessage ="Rating must be between 1 and 5 stars")]
         public int Rating { get; set; }
         public virtual Reviewer Reviewer { get; set; }
         public virtual Book Book { get; set; }
--- BookStoreAPI/Models/Reviewer.cs
+++ BookStoreAPI/Models/Reviewer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,18 @@ namespace BookStoreAPI.Models
 {
     public class Reviewer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100,ErrorMessage ="FirstName cannot be more than 100 characters")]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(200, ErrorMessage = "FirstName cannot be more than 200 characters")]
         public string LastName { get; set; }
+
         public virtual ICollection<Review> Reviews { get; set; }
     }
 }
